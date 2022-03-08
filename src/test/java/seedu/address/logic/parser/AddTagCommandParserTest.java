52b31e6c9906f6c5fd2d9dc480eeef50495b3be1
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-03-08 12:07:06 +0800

    Add test cases to fully resolve #36
    Test cases are added to ensure that the application has no regression
    after the previous commit that resolve #36.
    
    Let's,
    * Add test method parseCommand_addTag to AddressBookParserTest
    * Add AddTagCommandParserTest
      * Add parse_noTag_failure()
      * Add parse_invalidTag_failure()
      * Add parse_noIndex_failure()
      * Add parse_invalidIndex_failure()
      * Add parse_allFieldsSpecified_success()
      * Add parse_multipleTag_acceptsLast()
    * Add AddTagCommandTest
      * Add constructor_anyFieldsNull_throwsNullPointerException()
      * Add execute_validIndexUnfilteredList_success()
      * Add execute_duplicatedTagUnfilteredList_throwsCommandException()
      * Add execute_invalidIndexUnfilteredList_throwsCommandException()
      * Add execute_validIndexFilteredList_success()
      * Add execute_duplicatedTagFilteredList_throwsCommandException()
      * Add execute_invalidIndexFilteredList_throwsCommandException()
      * Add equals()
    * Add addTags to PersonBuilder to allow adding of tags, not overwriting
    * Add INDEX_FOURTH_PERSON to TypicalIndexes

---
+++ src/test/java/seedu/address/logic/parser/AddTagCommandParserTest.java
@@ -0,0 +1,91 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.AddTagCommand;
+import seedu.address.model.tag.Tag;
+
+class AddTagCommandParserTest {
+
+    private static final String TAG_EMPTY = " " + PREFIX_TAG;
+
+    private static final String MESSAGE_INVALID_FORMAT =
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTagCommand.MESSAGE_USAGE);
+
+    private AddTagCommandParser parser = new AddTagCommandParser();
+
+    @Test
+    public void parse_noTag_failure() {
+        assertParseFailure(parser, "1", MESSAGE_INVALID_FORMAT);
+    }
+
+    @Test
+    public void parse_invalidTag_failure() {
+        assertParseFailure(parser, "1" + TAG_EMPTY, Tag.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, "1" + TAG_EMPTY + "hubby*", Tag.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, "1" + TAG_EMPTY + "hubby and me", Tag.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, "1" + TAG_EMPTY + "hubby and me" + TAG_EMPTY, Tag.MESSAGE_CONSTRAINTS);
+    }
+
+    @Test
+    public void parse_noIndex_failure() {
+        assertParseFailure(parser, "", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, TAG_EMPTY, MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, TAG_EMPTY + "hubby*", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, TAG_EMPTY + "hubby and me", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, TAG_EMPTY + "hubby and me" + TAG_EMPTY, MESSAGE_INVALID_FORMAT);
+    }
+
+    @Test
+    public void parse_invalidIndex_failure() {
+        assertParseFailure(parser, "string" + TAG_EMPTY + VALID_TAG_FRIEND, MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "*(" + TAG_EMPTY + VALID_TAG_FRIEND, MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "*(12sz" + TAG_EMPTY + VALID_TAG_FRIEND, MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "0" + TAG_EMPTY + VALID_TAG_FRIEND, MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "-1" + TAG_EMPTY + VALID_TAG_FRIEND, MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "1 1" + TAG_EMPTY + VALID_TAG_FRIEND, MESSAGE_INVALID_FORMAT);
+    }
+
+    @Test
+    public void parse_allFieldsSpecified_success() {
+        // vanilla
+        AddTagCommand expectedCommand = new AddTagCommand(INDEX_FIRST_PERSON, new Tag(VALID_TAG_FRIEND));
+        assertParseSuccess(parser, INDEX_FIRST_PERSON.getOneBased() + TAG_EMPTY + VALID_TAG_FRIEND, expectedCommand);
+
+        // trailing and leading whitespaces
+        expectedCommand = new AddTagCommand(INDEX_FIRST_PERSON, new Tag(VALID_TAG_FRIEND));
+        assertParseSuccess(parser, " \n \t  " + INDEX_FIRST_PERSON.getOneBased() + TAG_EMPTY
+                + VALID_TAG_FRIEND + " \n \t  ", expectedCommand);
+
+        // leading spaces in tag
+        expectedCommand = new AddTagCommand(INDEX_FIRST_PERSON, new Tag(VALID_TAG_FRIEND));
+        assertParseSuccess(parser, "  \n \t " + INDEX_FIRST_PERSON.getOneBased() + TAG_EMPTY + " \n \t  "
+                + VALID_TAG_FRIEND + " \n \t  ", expectedCommand);
+    }
+
+    @Test
+    public void parse_multipleTag_acceptsLast() {
+        // two different tags
+        AddTagCommand expectedCommand = new AddTagCommand(INDEX_FIRST_PERSON, new Tag(VALID_TAG_FRIEND));
+        assertParseSuccess(parser, INDEX_FIRST_PERSON.getOneBased() + TAG_EMPTY + VALID_TAG_HUSBAND
+                + TAG_EMPTY + VALID_TAG_FRIEND, expectedCommand);
+
+        // two same tags
+        expectedCommand = new AddTagCommand(INDEX_FIRST_PERSON, new Tag(VALID_TAG_FRIEND));
+        assertParseSuccess(parser, INDEX_FIRST_PERSON.getOneBased() + TAG_EMPTY + VALID_TAG_FRIEND
+                + TAG_EMPTY + VALID_TAG_FRIEND, expectedCommand);
+
+        // three tags
+        expectedCommand = new AddTagCommand(INDEX_FIRST_PERSON, new Tag(VALID_TAG_HUSBAND));
+        assertParseSuccess(parser, INDEX_FIRST_PERSON.getOneBased() + TAG_EMPTY + VALID_TAG_FRIEND
+                + TAG_EMPTY + VALID_TAG_FRIEND + TAG_EMPTY + VALID_TAG_HUSBAND, expectedCommand);
+    }
+}
