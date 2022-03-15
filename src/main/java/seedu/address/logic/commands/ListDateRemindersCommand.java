8db1ae73a2062ae007c1628416129ee09bfc442d
cashewnade <e0559400@u.nus.edu>
2022-03-16 01:43:41 +0800

    Add commands to view reminders
    The current Reminder system implementation does not provide a way to display reminders. 
    
    This commit will provide two ways to view the reminders. One way is to specify the contact index, in which all of the reminders associated with the specified contact will be displayed. Otherwise users can choose to specify a date, in which all of the reminders happening on the specified date will be displayed. In the event which the date is not given, reminders happening today will be shown instead.
    
    Let's
    * Add ListDateReminderCommand to execute list reminders by date command.
    * Add ListDateReminderCommandParser to parse user inputs to ListDateReminderCommand.
    * Add ListContactReminderCommand to execute list reminders by contact command.
    * Add ListContactReminderCommandParser to parse user inputs to ListContactReminderCommand.
    
    This commit will close AY2122S2-CS2103T-T17-3#50

---
+++ src/main/java/seedu/address/logic/commands/ListDateRemindersCommand.java
@@ -0,0 +1,58 @@
+package seedu.address.logic.commands;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.date.ReminderDate;
+import seedu.address.model.person.Person;
+import seedu.address.model.reminder.Reminder;
+import seedu.address.model.reminder.ReminderList;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+
+/**
+ * Lists all reminders happening on a specified date.
+ */
+public class ListDateRemindersCommand extends Command {
+    public static final String COMMAND_WORD = "reminders";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all reminders on the specified date. "
+            + "Parameters: "
+            + PREFIX_DATE + "DATE\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_DATE + "2022-03-15\n";
+
+    public static final String MESSAGE_SUCCESS = "Listed all reminders on the date %1$s:\n %2$s";
+
+    private final ReminderDate reminderDate;
+
+    /**
+     * Creates a ListDateRemindersCommand to list the reminders.
+     */
+    public ListDateRemindersCommand(ReminderDate reminderDate) {
+        this.reminderDate = reminderDate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        StringBuilder output = new StringBuilder();
+        int counter = 1;
+        ObservableList<Person> personList = FXCollections.observableArrayList(model.getAddressBook().getPersonList());
+
+        for (Person person : personList) {
+
+            ReminderList temp = person.getReminderList().sameDateAs(reminderDate);
+
+            for (Reminder reminder : temp.getPriorityQueue()) {
+
+                output.append(String.format("%1$d. %2$s (%3$s)\n", counter, reminder.toString(),
+                        person.getName().toString()));
+            }
+        }
+
+        return new CommandResult(String.format(MESSAGE_SUCCESS, reminderDate.toString(), output.toString()));
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/ListContactRemindersCommandParser.java
@@ -0,0 +1,33 @@
+package seedu.address.logic.parser;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.ListContactRemindersCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+/**
+ * Parses input arguments and creates a new ListContactRemindersCommand object.
+ */
+public class ListContactRemindersCommandParser implements Parser<ListContactRemindersCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the ListContactRemindersCommand
+     * and returns a ListContactRemindersCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ListContactRemindersCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(args);
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    ListContactRemindersCommand.MESSAGE_USAGE), pe);
+        }
+
+        return new ListContactRemindersCommand(index);
+    }
+}
