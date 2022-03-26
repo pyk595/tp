e2d23966e17e10dd3d2287151a393574915eecec
pangyuanker <e0660595@u.nus.edu>
2022-03-27 00:51:25 +0800

    Change MESSAGE_EMPTY_CONTACTED_INFORMATION

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -10,7 +10,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
-    public static final String MESSAGE_EMPTY_CONTACTED_INFORMATION = "No logs of previous contact";
+    public static final String MESSAGE_EMPTY_CONTACTED_INFORMATION = "You have not contacted this person before";
 
 
 }
