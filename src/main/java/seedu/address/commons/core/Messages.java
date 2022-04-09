b9b34769e532eb6d010d7e83b6971c6f96492136
pangyuanker <e0660595@u.nus.edu>
2022-04-09 14:37:46 +0800

    Fix checkstyle

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -9,7 +9,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_ARGUMENTS = "Arguments are not allowed after this command word!";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid. "
-            +  "Please check if there is a person at this index.";
+            + "Please check if there is a person at this index.";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_EMPTY_CONTACTED_INFORMATION = "You have not contacted this person before";
 
